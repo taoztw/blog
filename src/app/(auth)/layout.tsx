b08be8e4a3fe5a181f66ec1c572ef42178eb9bc9
tz <taozhu@dip-ai.com>
2025-07-21 01:45:14 +0800

    删除了部分错误位置文件

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
 import type React from "react";
 
 const AuthLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-transparent">{children}</main>
-  );
+	return (
+		<main className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-transparent">{children}</main>
+	);
 };
 
 export default AuthLayout;
--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "button";
 import Link from "next/link";
 import React from "react";
 import { useForm } from "react-hook-form";
