2c1cc33475b90a62269076e6ab390f30f415b81b
tz <taozhu@dip-ai.com>
2025-08-03 01:47:19 +0800

    添加一些seo信息

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,13 @@
 import SignInForm from "@/components/forms/SignInForm";
 import { auth } from "@/server/auth";
+import type { Metadata } from "next";
 import { redirect } from "next/navigation";
 import React from "react";
+
+export const metadata: Metadata = {
+	title: "Sign In",
+	description: "Sign in to your account"
+};
 
 const SignIn = async () => {
 	const session = await auth();
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -3,6 +3,11 @@ import { auth } from "@/server/auth";
 import { redirect } from "next/navigation";
 import React from "react";
 
+export const metadata = {
+	title: "Sign Up",
+	description: "Create a new account"
+};
+
 const SignUp = async () => {
 	const session = await auth();
 	if (session?.user) {
